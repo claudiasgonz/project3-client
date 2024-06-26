ed2a4a19a5c37f5084d55278244fe638a8d7f137
Claudia G <claudsain@gmail.com>
2024-06-26 18:01:59 -0400

    marquee in about pg done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,32 +24,33 @@ function About() {
   </div>
 </div> */}
 
-<p className="font-mono p-5"
+<p className="font-mono px-5 py-10"
       >Muse.io is a single-page application (SPA) built with React, enabling users to explore museums and their details. The app features user authentication with encrypted passwords, allowing users to create accounts, log in, add reviews to museums, and delete their reviews. <br></br>
       <br></br>
       Admins have the capability to manage museum entries by creating, editing, and deleting them. The backend, developed with ExpressJS, MongoDB, and Mongoose, provides a REST API with routes for CRUD actions across three database models: users, museums, and reviews. The app includes backend validation, centralized error handling, and is styled using Tailwind CSS.
       </p>
 
-      <div className="relative flex overflow-x-hidden font-mono font-bold border-b-2 border-t-2 border-black">
-        <div className="py-5 animate-marquee whitespace-nowrap">
-          <Link to="https://github.com/claudiasgonz/project3-client" className="text-3xl mx-4 hover:text-blue-600">
+      <div className="relative flex overflow-x-hidden font-mono border-b-2 border-t-2 border-black">
+        <div className="py-4 animate-marquee whitespace-nowrap">
+          <Link to="https://github.com/claudiasgonz/project3-client" className="mx-4 hover:text-blue-600">
             github repo: frontend react app
           </Link>
-          <span className="text-4xl mx-4">♡</span>
-          <Link to="https://github.com/claudiasgonz/project3-server" className="text-3xl mx-4 hover:text-blue-600">
+          <span className="mx-4">♡</span>
+          <Link to="https://github.com/claudiasgonz/project3-server" className="mx-4 hover:text-blue-600">
             github repo: backend rest api
           </Link>
-          <span className="text-4xl mx-4">♡</span>
+          <span className="mx-4">♡</span>
         </div>
-        <div className="absolute top-0 py-5 animate-marquee2 whitespace-nowrap">
-          <Link to="https://github.com/claudiasgonz/project3-client" className="text-3xl mx-4 hover:text-blue-600">
+        <div className="absolute top-0 py-4 animate-marquee2 whitespace-nowrap">
+          <Link to="https://github.com/claudiasgonz/project3-client" className=" mx-4 hover:text-blue-600">
             github repo: frontend react app
           </Link>
-          <span className="text-4xl mx-4">♡</span>
-          <Link to="https://github.com/claudiasgonz/project3-server" className="text-3xl mx-4 hover:text-blue-600">
+
+          <span className="mx-4">♡</span>
+          <Link to="https://github.com/claudiasgonz/project3-server" className="mx-4 hover:text-blue-600">
             github repo: backend rest api
           </Link>
-          <span className="text-xl mx-4">♡</span>
+          <span className="mx-4">♡</span>
         </div>
       </div>
 
